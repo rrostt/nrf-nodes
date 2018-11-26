0dbabb7c524fb3b1780c91ac66541497c9b676f9
Mattias Rost <rrostt@gmail.com>
2018-11-26 23:08:06 +0100

    Add sleep mode to tempSensor

--- tempSensor/tempSensor.ino
+++ tempSensor/tempSensor.ino
@@ -1,5 +1,3 @@
-//#include <SPI.h>
-//#include <nRF24L01.h>
 #include <RF24.h>
 
 /*
@@ -23,13 +21,17 @@
 
 // ARDUINO is 2
 // ATTINY84 is 9
+#ifdef ATTINY_CORE
 #define ONE_WIRE_BUS 9
+#else
+#define ONE_WIRE_BUS 2
+#endif
 
 OneWire oneWire(ONE_WIRE_BUS);
 DallasTemperature sensors(&oneWire);
 
 // SENSOR CONFIG
-#define SENSOR_ID 102
+#define SENSOR_ID 100
 #define SENSOR_TYPE 1
 
 // #define DEBUG
@@ -37,12 +39,54 @@ DallasTemperature sensors(&oneWire);
 // RADIO
 // ARDUINO is 9 and 8
 // ATTINY84 is 8 and 7
+#ifdef ATTINY_CORE
 #define CE_PIN  8
 #define CSN_PIN 7
+#else
+#define CE_PIN  9
+#define CSN_PIN 8
+#endif
 
 RF24 radio(CE_PIN,CSN_PIN);
 
 byte gateway[5] = "ABCDE";
+
+// sleep
+#include <avr/sleep.h>
+#include <avr/wdt.h>
+#include <avr/interrupt.h>
+
+void setup_watchdog(int ii) {
+  // 0=16ms, 1=32ms,2=64ms,3=128ms,4=250ms,5=500ms
+  // 6=1 sec,7=2 sec, 8=4 sec, 9= 8sec
+
+  uint8_t bb;
+  if (ii > 9 ) ii=9;
+  bb=ii & 7;
+  if (ii > 7) bb|= (1<<5);
+  bb|= (1<<WDCE);
+
+  MCUSR &= ~(1<<WDRF);
+  // start timed sequence
+  WDTCSR |= (1<<WDCE) | (1<<WDE);
+  // set new watchdog timeout value
+  WDTCSR = bb;
+  WDTCSR |= _BV(WDIE);
+}
+
+void system_sleep() {
+  setup_watchdog(9);                   // approximately 8 seconds sleep
+ 
+  set_sleep_mode(SLEEP_MODE_PWR_DOWN); // sleep mode is set here
+  sleep_enable();
+  sei();                               // Enable the Interrupts so the wdt can wake us up
+
+  sleep_mode();                        // System sleeps here
+
+  sleep_disable();                     // System continues execution here when watchdog timed out 
+}
+
+ISR(WDT_vect) {}
 
 void setup() {
 #ifdef DEBUG
@@ -55,16 +99,27 @@ void setup() {
   radio.begin();
   radio.setDataRate( RF24_250KBPS );
   radio.setPALevel(RF24_PA_HIGH);
+  radio.setRetries(15,15);
 
   radio.openWritingPipe(gateway);
 }
 
 uint8_t message[32];
+int counter = 5;
 
 void loop() {
-  prepareMessage(getTemperature()*100);
-  sendMessage();
-  delay(200);
+  counter++;
+  if (counter >= 6) {
+    radio.powerUp();
+    prepareMessage(getTemperature()*100);
+    sendMessage();
+    counter = 0;
+
+    radio.powerDown();
+    delay(50);
+  }
+
+  system_sleep();
 }
 
 void prepareMessage(float temp) {
